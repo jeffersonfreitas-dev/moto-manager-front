2e0ccbd1ba1d694c3b886b4b6d917f28126fdd44
Antonio Jefferson Moreira Freitas <jeffersonfreitas@unimedce.com.br>
2023-11-05 16:07:35 -0300

    Adicionado deploy firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: 'https://jeffersonfreitas.dev/api/v1',
+  apiURL: 'https://jeffersonfreitas.dev/motomanager/api/v1',
   keycloakURL: 'https://jeffersonfreitas.dev/auth',
   url: 'https://motomanager.jeffersonfreitas.dev'
 };
